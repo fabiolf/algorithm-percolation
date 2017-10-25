94c2804eb37766ecc3ff4faebbb7e3f993fb3bcf
Fabio Fonseca <fonseca@ge.com>
2017-10-25 16:07:40 -0200

    trying to solve backwash problem

--- src/Percolation.java
+++ src/Percolation.java
@@ -5,8 +5,7 @@ public class Percolation {
     private final int mSize;
     private boolean[] mBoolGrid;
     private int mOpenSites = 0;
-    private boolean mBottomConnected = false;
-//    private boolean mPercolates = false;
+    private boolean mPercolated = false;
     
     // create n-by-n grid, with all sites blocked
     public Percolation(int n) {
@@ -14,10 +13,10 @@ public class Percolation {
         if (n <= 0)
             throw new java.lang.IllegalArgumentException();
             
-        mUf = new WeightedQuickUnionUF(n*n+2);
+        mUf = new WeightedQuickUnionUF(n*n+3);
 
         mSize = n;
-        mBoolGrid = new boolean[n*n+2];
+        mBoolGrid = new boolean[n*n+3];
         mBoolGrid[0] = true;
         mBoolGrid[n*n+1] = true;
     }
@@ -58,9 +57,9 @@ public class Percolation {
             if (col > 1 && col <= mSize && mBoolGrid[elementIndex-1])
                 mUf.union(elementIndex, elementIndex-1);
             
-            // connects to the bottom virtual element
-            if (row == mSize && !mBottomConnected && isFull(row, col)) {
-                mBottomConnected = true;
+            // connects to the bottom virtual element if it is full (percolates to the top)
+            // and it is the first element to do it.
+            if (row == mSize) {
                 mUf.union(mSize*mSize+1, elementIndex);
             }
         }
@@ -75,6 +74,8 @@ public class Percolation {
     
     // is site (row, col) full?
     public boolean isFull(int row, int col) {
+        if (row <= 0 || row > mSize || col <= 0 || col > mSize)
+            throw new java.lang.IllegalArgumentException();
         boolean isConnected = mUf.connected(ijto1d(row, col), 0);
         return (isOpen(row, col) && isConnected);
     }
@@ -86,31 +87,50 @@ public class Percolation {
     
     // does the system percolate?
     public boolean percolates() {
-        return mUf.connected(0, mSize*mSize+1);
+        // when it is not percolating, always test all opened bottom sites
+        // when it begins percolating, just retrieve a variable
+        if (!mPercolated) {
+            mPercolated = mUf.connected(0, mSize*mSize+1);
+            // the first time it percolates
+            // check if any opened bottom site is full
+            if (mPercolated) {
+                // connects the first full site with the new bottom site
+                // and uses it from now on
+                for (int i = 1; i <= mSize; i++) {
+                    if (isOpen(mSize, i) && isFull(mSize, i)) {
+                        mUf.union(ijto1d(mSize, i), mSize*mSize+2);
+                        break;
+                    }
+                }
+            }
+            return mPercolated;
+        }
+        else
+            return mUf.connected(0, mSize*mSize+2);
     }
     
-    public void printGrid() {
-        for (int i = 1; i <= mSize; i++) {
-            String line = "";
-            for (int j = 1; j <= mSize; j++) {
-                line += (mBoolGrid[ijto1d(i, j)] ? "O" : "X");
-                line += "  ";
-            }
-            System.out.println(line);
-        }
-        System.out.println("Open sites: " + numberOfOpenSites());
-        System.out.println("Percolates? " + percolates());
-    }
+//    public void printGrid() {
+//        for (int i = 1; i <= mSize; i++) {
+//            String line = "";
+//            for (int j = 1; j <= mSize; j++) {
+//                line += (mBoolGrid[ijto1d(i, j)] ? "O" : "X");
+//                line += "  ";
+//            }
+//            System.out.println(line);
+//        }
+//        System.out.println("Open sites: " + numberOfOpenSites());
+//        System.out.println("Percolates? " + percolates());
+//    }
 
     // test client (optional)
     public static void main(String[] args) {
         Percolation per1 = new Percolation(3);
         System.out.println("All sites blocked");
-        per1.printGrid();
+//        per1.printGrid();
 //        System.out.println("Adding 1 openings");
         per1.open(1, 3);
         per1.open(2, 3);
         per1.open(3, 3);
-        per1.printGrid();
+//        per1.printGrid();
     }
 }
